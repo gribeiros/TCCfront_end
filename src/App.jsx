4952b9ed9cb1809540411e3b15662e7a632923db
Gabriel Duarte Jr <gabriel181193@hotmail.com>
2022-11-09 20:17:23 -0300

    chore; ajust usememo value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
 import './App.css';
@@ -7,7 +7,8 @@ const socket = io.connect('http://localhost:8080');
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [arduinoStatus, setArduinoStatus] = useState({});
+  const [arduinoWaterLevel, setArduinoWaterLevel] = useState(null);
+  const [arduinoFlowLevel, setArduinoFlowLevel] = useState(null);
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -18,10 +19,10 @@ function App() {
       setIsConnected(false);
     });
 
-    socket.on('arduinoStatus', async (data) => {
-      await setArduinoStatus(data.status);
+    socket.on('arduinoStatus', (data) => {
+      setArduinoWaterLevel(data.status.waterLevel);
+      setArduinoFlowLevel(data.status.flowLevel);
     });
-
 
     return () => {
       socket.off('connect');
@@ -32,39 +33,42 @@ function App() {
 
   const sendRequest = async () => {
     try {
-      await axios.get('http://localhost:8080/update-status').then(
-        function (response) {
-          setArduinoStatus(response.data.status)
-          console.log('SEND ' + JSON.stringify(response.data.status))
-        }
-      )
+      const { data } = await axios.get('http://localhost:8080/update-status');
+      setArduinoWaterLevel(data.status.waterLevel);
+      setArduinoFlowLevel(data.status.flowLevel);
+      console.log('SEND ' + JSON.stringify(data.status));
     } catch (error) {
       console.error(error);
     }
   }
 
-  const connectedValue = useMemo(() => {
+  const connected = useMemo(() => {
     if (isConnected) return 'Conectado';
     return 'Não conectado';
   }, [isConnected])
 
-  const arduinoStatusValue = useMemo(() => {
-    if (arduinoStatus) return `${arduinoStatus}`;
+  const waterLevel = useMemo(() => {
+    if (arduinoWaterLevel) return `${arduinoWaterLevel}`;
     return '--';
-  }, [arduinoStatus])
+  }, [arduinoWaterLevel])
+
+  const flowLevel = useMemo(() => {
+    if (arduinoFlowLevel) return `${arduinoFlowLevel}`;
+    return '--';
+  }, [arduinoFlowLevel])
 
   return (
     <div className='card'>
       <p className='title'>Sensor de nível de água</p>
 
       <p>Status da conexão:</p>
-      <p className='connected-value'>{connectedValue}</p>
+      <p className='connected-value'>{connected}</p>
 
       <p>Nível da água:</p>
-      <p className='info-value'>{arduinoStatus.waterLevel}</p>
+      <p className='info-value'>{waterLevel}</p>
 
       <p>Fluxo e água:</p>
-      <p className='info-value'>{arduinoStatus.flowLevel}</p>
+      <p className='info-value'>{flowLevel}</p>
 
       <button onClick={sendRequest}>Atualizar</button>
     </div>
